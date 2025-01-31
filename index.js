cc73ddd79c4f9e4e0378e16792e0f864119ff800
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-01-31 23:06:40 +0800

    MultiChainBalanceChecking

--- index.js
+++ index.js
@@ -26,35 +26,76 @@ async function fetchPrices(symbols) {
 const celestiaAddress = "celestia18pl8klprl8ktqrwn86chw8l56kcw0mapp3x88x"
 const address = celestiaAddress
 
-// Convert address type
+// Address symbol for address reconstruction
 const mapNetworkToSymbol = {
     celestia: "celestia",
     cosmoshub: "cosmos",
     osmosis: "osmo"
 }
 
+// Address symbol for address reconstruction
+const mapNetworkToTokenName = {
+    celestia: "tia",
+    cosmoshub: "atom",
+    osmosis: "osmo"
+}
+
+// cosmos addresses
 const selectedNetworks = ["celestia", "cosmoshub", "osmosis"]
-const networkAddresses = []
-selectedNetworks.forEach(cosmosNetwork => networkAddresses.push({networkName: cosmosNetwork, address: convertAddress(celestiaAddress, mapNetworkToSymbol[cosmosNetwork])}))
-console.log(networkAddresses)
 
-// Single address balance
-const cosmosNetwork = "celestia"
-const baseCosmosUrl = `https://rest.cosmos.directory/${cosmosNetwork}/cosmos`
+const networkAddresses = selectedNetworks.map(cosmosNetwork => {
+        return {
+            networkName: cosmosNetwork, 
+            tokenName: mapNetworkToTokenName[cosmosNetwork],
+            address: convertAddress(celestiaAddress, mapNetworkToSymbol[cosmosNetwork])
+        }
+    }
+)
 
-const liquidBalancePromise = fetch(`${baseCosmosUrl}/bank/v1beta1/balances/${address}`)
-const stakingBalancePromise = fetch(`${baseCosmosUrl}/staking/v1beta1/delegations/${address}`)
-const rewardBalancePromise = fetch(`${baseCosmosUrl}/distribution/v1beta1/delegators/${address}/rewards`)
+networkAddresses.map(network => {
+    return fetchBalance(network)
+})
 
-const DataPromise = await Promise.all([liquidBalancePromise, stakingBalancePromise, rewardBalancePromise])
-const data = await Promise.all(DataPromise.map(response => response.json()))
+async function fetchBalance(cosmosNetwork) {
+    const {networkName, tokenName, address} = cosmosNetwork
+    const baseCosmosUrl = `https://rest.cosmos.directory/${networkName}/cosmos`
 
-const liquidBalance = Number(data[0].balances[0].amount)
-const stakingBalance = Number(data[1].delegation_responses[0].balance.amount)
-const rewardBalance = Number(data[2].rewards[0].reward[0].amount)
+    const DataPromise = await Promise.all([
+        fetch(`${baseCosmosUrl}/bank/v1beta1/balances/${address}`), 
+        fetch(`${baseCosmosUrl}/staking/v1beta1/delegations/${address}`), 
+        fetch(`${baseCosmosUrl}/distribution/v1beta1/delegators/${address}/rewards`)]
+    )
 
-const totalBalance = ((liquidBalance + stakingBalance + rewardBalance) / 1000000).toFixed(6)
-console.log({tia: totalBalance})
+    const [liquidData, stakingData, rewardData] = await Promise.all(DataPromise.map(response => response.json()))
+
+    const liquidBalance = Number(liquidData.balances.find(balances => balances.denom === "u" + tokenName).amount)
+    const stakingBalance = stakingData.delegation_responses.length > 0 ? Number(stakingData.delegation_responses[0].balance.amount) : 0
+    const rewardBalance = rewardData.rewards.length > 0 ? Number(rewardData.rewards[0].reward[0].amount) : 0
+    
+    console.log({liquidBalance, stakingBalance, rewardBalance})
+
+    const totalBalance = ((liquidBalance + stakingBalance + rewardBalance) / 1000000).toFixed(6)
+    console.log({network: networkName, token: tokenName, balance: totalBalance})
+
+}
+
+// // Single address balance
+// const cosmosNetwork = "celestia"
+// const baseCosmosUrl = `https://rest.cosmos.directory/${cosmosNetwork}/cosmos`
+
+// const liquidBalancePromise = fetch(`${baseCosmosUrl}/bank/v1beta1/balances/${address}`)
+// const stakingBalancePromise = fetch(`${baseCosmosUrl}/staking/v1beta1/delegations/${address}`)
+// const rewardBalancePromise = fetch(`${baseCosmosUrl}/distribution/v1beta1/delegators/${address}/rewards`)
+
+// const DataPromise = await Promise.all([liquidBalancePromise, stakingBalancePromise, rewardBalancePromise])
+// const data = await Promise.all(DataPromise.map(response => response.json()))
+
+// const liquidBalance = Number(data[0].balances[0].amount)
+// const stakingBalance = Number(data[1].delegation_responses[0].balance.amount)
+// const rewardBalance = Number(data[2].rewards[0].reward[0].amount)
+
+// const totalBalance = ((liquidBalance + stakingBalance + rewardBalance) / 1000000).toFixed(6)
+// console.log({tia: totalBalance})
 
 
 
