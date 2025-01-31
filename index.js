9698accd9daa0ec77060286aa0f141bcec169b88
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-01-31 22:06:08 +0800

    remove unnessary console.log

--- index.js
+++ index.js
@@ -23,11 +23,13 @@ async function fetchPrices(symbols) {
 //     console.log("Stop fetching")
 // }, 20000)
 
+const address = "celestia1fzq595eyhdnstdm7g2stvqkj7fj99l5hehhvje"
+console.log("Osmosis Address:", convertAddress(address, "osmo"))
+
 // Convert address type
 const cosmosNetwork = "celestia"
 const baseCosmosUrl = `https://rest.cosmos.directory/${cosmosNetwork}/cosmos`
-const address = "celestia1fzq595eyhdnstdm7g2stvqkj7fj99l5hehhvje"
-console.log("Osmosis Address:", convertAddress(address, "osmo"))
+
 
 Promise.all([])
 const liquidBalancePromise = fetch(`${baseCosmosUrl}/bank/v1beta1/balances/${address}`)
@@ -36,14 +38,10 @@ const rewardBalancePromise = fetch(`${baseCosmosUrl}/distribution/v1beta1/delega
 
 const DataPromise = await Promise.all([liquidBalancePromise, stakingBalancePromise, rewardBalancePromise])
 const data = await Promise.all(DataPromise.map(response => response.json()))
-console.log(data)
 
 const liquidBalance = Number(data[0].balances[0].amount)
 const stakingBalance = Number(data[1].delegation_responses[0].balance.amount)
 const rewardBalance = Number(data[2].rewards[0].reward[0].amount)
-console.log({liquidBalance})
-console.log({stakingBalance})
-console.log({rewardBalance})
 
 const totalBalance = ((liquidBalance + stakingBalance + rewardBalance) / 1000000).toFixed(6)
 console.log({tia: totalBalance})
