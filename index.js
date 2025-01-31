57ab3baf207a1659bc28cd80ff1fe0382771023d
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-01-31 23:19:07 +0800

    remove legacy codes and added more output info

--- index.js
+++ index.js
@@ -66,26 +66,5 @@ async function fetchBalance(cosmosNetwork) {
 
     const totalBalance = ((liquidBalance + stakingBalance + rewardBalance)).toFixed(6)
 
-    return {network: networkName, token: tokenName, balance: totalBalance}
+    return {network: networkName, address: address, token: tokenName, balance: totalBalance}
 }
-
-// // Single address balance
-// const cosmosNetwork = "celestia"
-// const baseCosmosUrl = `https://rest.cosmos.directory/${cosmosNetwork}/cosmos`
-
-// const liquidBalancePromise = fetch(`${baseCosmosUrl}/bank/v1beta1/balances/${address}`)
-// const stakingBalancePromise = fetch(`${baseCosmosUrl}/staking/v1beta1/delegations/${address}`)
-// const rewardBalancePromise = fetch(`${baseCosmosUrl}/distribution/v1beta1/delegators/${address}/rewards`)
-
-// const DataPromise = await Promise.all([liquidBalancePromise, stakingBalancePromise, rewardBalancePromise])
-// const data = await Promise.all(DataPromise.map(response => response.json()))
-
-// const liquidBalance = Number(data[0].balances[0].amount)
-// const stakingBalance = Number(data[1].delegation_responses[0].balance.amount)
-// const rewardBalance = Number(data[2].rewards[0].reward[0].amount)
-
-// const totalBalance = ((liquidBalance + stakingBalance + rewardBalance) / 1000000).toFixed(6)
-// console.log({tia: totalBalance})
-
-
-
